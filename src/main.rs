cc2d243aab1ad1f45c07c71babdf1347e1abf266
ttyS3 <ttys3.rust@gmail.com>
2022-11-27 22:13:32 +0800

    chore: format main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,38 @@
-use axum::{routing::get, Router, middleware};
-use std::net::SocketAddr;
 use axum::http::Request;
 use axum::middleware::Next;
 use axum::response::Response;
+use axum::{middleware, routing::get, Router};
+use std::net::SocketAddr;
 use tower::ServiceBuilder;
 
 #[tokio::main]
 async fn main() {
-    // let app = Router::new().route("/", get(handler))
-    // .layer(middleware::from_fn(my_middleware1))
-    // .layer(middleware::from_fn(my_middleware2))
-    // .layer(middleware::from_fn(my_middleware3));
+    // let app = Router::new()
+    //     .route("/", get(handler))
+    //     .layer(middleware::from_fn(my_middleware1))
+    //     .layer(middleware::from_fn(my_middleware2))
+    //     .layer(middleware::from_fn(my_middleware3));
 
     // https://docs.rs/axum/latest/axum/middleware/index.html#ordering
     // When you add middleware with Router::layer (or similar) all previously added routes will be wrapped in the middleware.
     // Generally speaking, this results in middleware being executed from bottom to top.
     /*
-        requests
-           |
-           v
-+------ middleware3 ------+
-| +---- middleware2 ----+ |
-| | +-- middleware1 --+ | |
-| | |               | | | |
-| | |    handler    | | | |
-| | |               | | | |
-| | +-- middleware1 --+ | |
-| +---- middleware2 ----+ |
-+------ middleware3 ------+
-           |
-           v
-        responses
-     */
+            requests
+               |
+               v
+    +------ middleware3 ------+
+    | +---- middleware2 ----+ |
+    | | +-- middleware1 --+ | |
+    | | |               | | | |
+    | | |    handler    | | | |
+    | | |               | | | |
+    | | +-- middleware1 --+ | |
+    | +---- middleware2 ----+ |
+    +------ middleware3 ------+
+               |
+               v
+            responses
+         */
     // output log:
     // before next.run: "middleware3"
     // before next.run: "middleware2"
@@ -43,33 +44,33 @@ async fn main() {
 
     // its recommended to use tower::ServiceBuilder to apply multiple middleware at once,
     // instead of calling layer (or route_layer) repeatedly
-    let app = Router::new().route("/", get(handler))
-        .layer(
-    ServiceBuilder::new()
-        .layer(middleware::from_fn(my_middleware1))
-        .layer(middleware::from_fn(my_middleware2))
-        .layer(middleware::from_fn(my_middleware3)));
+    let app = Router::new().route("/", get(handler)).layer(
+        ServiceBuilder::new()
+            .layer(middleware::from_fn(my_middleware1))
+            .layer(middleware::from_fn(my_middleware2))
+            .layer(middleware::from_fn(my_middleware3)),
+    );
 
     // but when use tower::ServiceBuilder to apply multiple middleware at once, the order is reversed to `Router::layer`
     // https://docs.rs/tower/0.4.13/tower/builder/struct.ServiceBuilder.html#order
     // Layers that are added first will be called with the request first.
     /*
-        requests
-           |
-           v
-+------ middleware1 ------+
-| +---- middleware2 ----+ |
-| | +-- middleware3 --+ | |
-| | |               | | | |
-| | |    handler    | | | |
-| | |               | | | |
-| | +-- middleware3 --+ | |
-| +---- middleware2 ----+ |
-+------ middleware1 ------+
-           |
-           v
-        responses
-     */
+            requests
+               |
+               v
+    +------ middleware1 ------+
+    | +---- middleware2 ----+ |
+    | | +-- middleware3 --+ | |
+    | | |               | | | |
+    | | |    handler    | | | |
+    | | |               | | | |
+    | | +-- middleware3 --+ | |
+    | +---- middleware2 ----+ |
+    +------ middleware1 ------+
+               |
+               v
+            responses
+         */
     // output log:
     // before next.run: "middleware1"
     // before next.run: "middleware2"
